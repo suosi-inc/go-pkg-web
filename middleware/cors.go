de7de5df04a635d33f7a654fd5cd3abffe57475c
smallmenu <smallmenu@163.com>
2024-11-22 10:09:19 +0800

    add: CorsWithDefaultOption

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,7 +9,7 @@ import (
 const (
 	CorsDefaultAllowOrigin      = "*"
 	CorsDefaultAllowMethods     = "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-	CorsDefaultAllowHeaders     = "Origin, Content-Type, X-CSRF-Token, Authorization, Jwt-Authorization, X-Requested-With, Accept, X-Cookie"
+	CorsDefaultAllowHeaders     = "Origin, Content-Type, X-CSRF-Token, X-Requested-With, Authorization, Jwt-Authorization, Accept"
 	CorsDefaultExposeHeaders    = "Content-Length, Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma"
 	CorsDefaultAllowCredentials = "true"
 	CorsDefaultMaxAge           = "86400"
@@ -41,6 +41,48 @@ func Cors() gin.HandlerFunc {
 		c.Next()
 	}
 
+}
+
+// CorsWithDefaultOption is a middleware function that appends headers to allow CORS with option, if option is empty, use default config
+func CorsWithDefaultOption(option CorsOption) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if option.AllowOrigin != "" {
+			c.Header("Access-Control-Allow-Origin", option.AllowOrigin)
+		} else {
+			c.Header("Access-Control-Allow-Origin", CorsDefaultAllowOrigin)
+		}
+		if option.AllowMethods != "" {
+			c.Header("Access-Control-Allow-Methods", option.AllowMethods)
+		} else {
+			c.Header("Access-Control-Allow-Methods", CorsDefaultAllowMethods)
+		}
+		if option.AllowHeaders != "" {
+			c.Header("Access-Control-Allow-Headers", option.AllowHeaders)
+		} else {
+			c.Header("Access-Control-Allow-Headers", CorsDefaultAllowHeaders)
+		}
+		if option.ExposeHeaders != "" {
+			c.Header("Access-Control-Expose-Headers", option.ExposeHeaders)
+		} else {
+			c.Header("Access-Control-Expose-Headers", CorsDefaultExposeHeaders)
+		}
+		if option.AllowCredentials != "" {
+			c.Header("Access-Control-Allow-Credentials", option.AllowCredentials)
+		} else {
+			c.Header("Access-Control-Allow-Credentials", CorsDefaultAllowCredentials)
+		}
+		if option.MaxAge != "" {
+			c.Header("Access-Control-Max-Age", option.MaxAge)
+		} else {
+			c.Header("Access-Control-Max-Age", CorsDefaultMaxAge)
+		}
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		c.Next()
+	}
 }
 
 // CorsWithOption is a middleware function that appends headers to allow CORS with option
